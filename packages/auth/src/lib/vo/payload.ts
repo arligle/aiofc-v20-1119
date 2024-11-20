3aef716dc5ccaae11e7adcb6a6a6ee26e1316343
arligle <0774543002@163.com>
2024-11-20 17:52:16 +0800

    feat(packages): created @aiofc/auth

---
+++ packages/auth/src/lib/vo/payload.ts
@@ -0,0 +1,42 @@
+export interface IAccessTokenPayload {
+  sub: string;
+  email: string;
+}
+
+export interface PermissionsBaseJwtPayload extends IAccessTokenPayload {
+  permissions: string[];
+}
+
+export interface IAccessTokenPayloadWithTenantsInfo<T>
+  extends IAccessTokenPayload {
+  tenants: TenantInfo<T>[];
+}
+
+export interface IAccessTokenSingleTenantPayload extends IAccessTokenPayload {
+  tenantId: string;
+}
+
+export interface RoleBasedAccessTokenPayload<T> extends IAccessTokenPayload {
+  roles: RoleInfo<T>[];
+}
+
+export interface IRefreshTokenPayload extends IAccessTokenPayload {}
+
+export interface TenantInfo<T> {
+  tenantId: string;
+  roles: RoleInfo<T>[];
+}
+
+export interface RoleInfo<T> {
+  roleId: string;
+  roleType?: T;
+}
+
+export interface RolesBaseJwtPayload<T> extends IAccessTokenPayload {
+  roles: RoleInfo<T>[];
+}
+
+export interface PayloadSigned {
+  iat: number;
+  exp: number;
+}
